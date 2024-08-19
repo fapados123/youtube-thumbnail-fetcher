c2d085c31ef4fdd309b722a808c0cb67e0f2f923
Bence Király <kiralybencepecs@gmail.com>
2024-08-19 23:24:28 +0200

    Small refactor

--- assets/app.js
+++ assets/app.js
@@ -1,72 +1,46 @@
-function getVideoUrl() {
-    return document.querySelector('#url-input').value;
-}
+const types = [
+    '0',
+    '1',
+    '2',
+    '3',
+    'hq1',
+    'hq2',
+    'hq3',
+    'hq720',
+    'hqdefault',
+    'mq1',
+    'mq2',
+    'mq3',
+    'mqdefault',
+    'sd1',
+    'sd2',
+    'sd3',
+    'sddefault',
+    'default',
+    'maxresdefault',
+];
 
-function getVideoId() {
-    return new URL(getVideoUrl()).searchParams.get('v');
-}
-
-function getThumbnailFormat() {
-    return document.querySelector('#format-selector').value;
-}
-
-function getPrefix(format) {
-    switch (format) {
-        case 'jpg':
-            return 'vi';
-
-        case 'webp':
-            return 'vi_webp';
-
-        default:
-            return 'vi';
-    }
-}
-
-function getTypes() {
-    return [
-        '0',
-        '1',
-        '2',
-        '3',
-        'hq1',
-        'hq2',
-        'hq3',
-        'hq720',
-        'hqdefault',
-        'mq1',
-        'mq2',
-        'mq3',
-        'mqdefault',
-        'sd1',
-        'sd2',
-        'sd3',
-        'sddefault',
-        'default',
-        'maxresdefault',
-    ];
-}
+const prefixes = {
+    jpg: 'vi',
+    webp: 'vi_webp',
+};
 
 function generateThumbnailUrl(videoId, type, format) {
-    return `https://i.ytimg.com/${getPrefix(format)}/${videoId}/${type}.${format}`;
-}
-
-function displayThumbnail(videoId, type, format) {
-    document.querySelector(`#thumbnail-${type}`).setAttribute('src', generateThumbnailUrl(videoId, type, format));
-}
-
-function showThumbnailContainer() {
-    document.querySelector('#thumbnail-container').style.display = 'initial';
-}
-
-function fetchThumbnails() {
-    getTypes().forEach(type => {
-        displayThumbnail(getVideoId(), type, getThumbnailFormat());
-    });
-
-    showThumbnailContainer();
+    return `https://i.ytimg.com/${prefixes[format]}/${videoId}/${type}.${format}`;
 }
 
 document.querySelector('#fetch-button').addEventListener('click', () => {
-    fetchThumbnails();
+    let videoUrl = document.querySelector('#url-input').value;
+    let videoId = new URL(videoUrl).searchParams.get('v');
+    let thumbnailFormat = document.querySelector('#format-selector').value;
+
+    types.forEach(type => {
+        let url = generateThumbnailUrl(videoId, type, thumbnailFormat);
+        
+        // Display thumbnail
+        document.querySelector(`#thumbnail-${type}`).setAttribute('src', url);
+    });
+
+    // Show thumbnail container
+    document.querySelector('#thumbnail-container').style.display = 'initial';
 });
